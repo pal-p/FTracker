b9b465b8881d116ec457d31d085636edc0e9b811
Xyli0 <xyli0dev.dev@gmail.com>
2018-11-22 16:59:18 +0100

    add account creation form

--- routes/account.js
+++ routes/account.js
@@ -3,6 +3,7 @@ const controller = require("../controllers/accounts");
 const validateToken = require("../utils").validateToken; //middleware to verify token
 const router = express.Router();
 
+router.get('/new', validateToken, controller.getNew);
 router.post('/new', validateToken, controller.create);
 
 module.exports = router;
